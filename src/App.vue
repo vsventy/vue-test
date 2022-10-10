7354e9189f030c8e0fcc13913a373172a41363d8
Vitalii Sventyi <vsventy@gmail.com>
2022-10-10 23:49:59 +0300

    add markup of app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,92 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="wrapper">
+    
+    <header class="header">
+      <div class="header__container">
+        <h1 class="header__title title ">We make creative media that <span >adds value</span></h1>
+      </div>
+    </header>
+    
+    <main class="page">
+      <div class="main__container flex"></div>
+    </main>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <footer class="footer">
+      <div class="footer__container">
+        <div class="footer__actions">
+          <button type="button" class="footer__button button">Get Started</button>
+        </div>
+      </div>
+    </footer>
 
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style lang="scss" scoped>
+
+.wrapper {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.header__container {
+  max-width: var(--section-heading);
+  box-sizing: content-box;
+  margin: 0 auto;
+  align-items: center;
+  text-align: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+.title {
+  font-family: Poppins;
+  font-weight: 700;
+  font-size: 3.5rem;
+  line-height: 3.4375rem;
+  color: var(--color-heading);
+  span {
+    color: var(--color-highlighted);
   }
 }
+
+.footer__container {
+  max-width: var(--max-width-container);
+  box-sizing: content-box;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.main__container {
+  margin: 5rem auto;
+}
+
+.button {
+  font-family: Poppins;
+  font-weight: 700;
+  font-size: 1.0625rem;
+  display: inline-block;
+  line-height: 3.75rem;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+  padding: 0px 5.1945rem;
+  background-color: #1b264f;
+  border-radius: 0.75rem;
+  border: none;
+  outline: none;
+  transition: all 0.3s ease 0s;
+  color: #fff;
+  @media (any-hover: hover) {
+    &:hover {
+      background-color: #121933;
+    }
+  }
+  &._fw {
+    width: 100%;
+  }
+}
+
 </style>
