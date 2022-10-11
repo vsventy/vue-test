eb67a1ac6e355ebdc6f63f409fe9a9877a413243
Vitalii Sventyi <vsventy@gmail.com>
2022-10-11 14:05:18 +0300

    add feature component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,96 @@
-<script setup>
+<script>
+import MediaFeature from "./components/MediaFeature.vue";
+
+export default {
+  components: {
+    MediaFeature,
+  },
+
+  data() {
+    return {
+      features: [
+        {
+          title: "Photography",
+          text: "Every photograph we produce finds the beauty in your property while serving as a profitable asset.",
+          url: "image_1_1.png",
+          url2: "image_1_2.png",
+          items: ["Drone Photography", "Interiors", "Exteriors / Architectural"]
+        },
+        {
+          title: "Virtual Staging",
+          text: "Our staging will help you sell the potential of your space.<br/>We can work off of our own photography or photos you provide.",
+          url: "image_2.png",
+        },
+        {
+          title: "Renderings",
+          text: "Renderings are the gold standard in pre-construction marketing.",
+          url: "image_3.png",
+          items: ["Interiors", "Exteriors"]
+        },
+        {
+          title: "Interactive Tours",
+          text: "Give your audience the ability to explore your property without requiring in-person travel.",
+          url: "image_4.png",
+          items: ["Matterport", "Video Walkthroughs"]
+        },
+        {
+          title: "Floor Plans",
+          text: "We produce laser-precise floor plans faster than any other service.",
+          url: "image_5.png",
+          items: ["On-site Measure", "2D Floor Plans", "3D Floor Plans"]
+        },
+        {
+          title: "Video",
+          text: "Video content has higher engagement and will help your property stand out online.",
+          url: "image_6.png",
+          items: ["Drone Cinematography", "Interiors", "Exteriors / Architectural"]
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <div class="wrapper">
-    
     <header class="header">
       <div class="header__container">
-        <h1 class="header__title title ">We make creative media that <span >adds value</span></h1>
+        <h1 class="header__title title">
+          We make creative media that <span>adds value</span>
+        </h1>
       </div>
     </header>
-    
+
     <main class="page">
-      <div class="main__container flex"></div>
+      <div class="main__container">
+        <section class="page__features">
+          <div class="features__container">
+            <MediaFeature
+              v-for="feature in features"
+              :title="feature.title"
+              :text="feature.text"
+              :url="feature.url"
+              :url2="feature.url2"
+              :items="feature.items"
+            />
+          </div>
+        </section>
+      </div>
     </main>
 
     <footer class="footer">
       <div class="footer__container">
         <div class="footer__actions">
-          <button type="button" class="footer__button button">Get Started</button>
+          <button type="button" class="footer__button button">
+            Get Started
+          </button>
         </div>
       </div>
     </footer>
-
   </div>
 </template>
 
 <style lang="scss" scoped>
-
 .wrapper {
   min-height: 100%;
   display: flex;
@@ -53,15 +117,23 @@
   }
 }
 
+.main__container {
+  margin: 5rem auto;
+}
+
+.features__container {
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 2.5rem;
+  column-gap: 1.875rem;
+  justify-content: center;
+}
+
 .footer__container {
   max-width: var(--max-width-container);
   box-sizing: content-box;
   margin: 0 auto;
   text-align: center;
-}
-
-.main__container {
-  margin: 5rem auto;
 }
 
 .button {
@@ -88,5 +160,4 @@
     width: 100%;
   }
 }
-
 </style>
